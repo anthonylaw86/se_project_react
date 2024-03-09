20fa021e1c891d8d58453f7747289d2f94484fbe
AnthonyLaw <anthonylaw910@gmail.com>
2024-03-09 16:46:08 -0700

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,15 @@
 function Header() {
-  return <header>HEADER</header>;
+  return (
+    <header className="header">
+      <img src="" alt="" className="header__logo" />
+      <p className="header__date-and-location">DATE, LOCATION</p>
+      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">NAME</p>
+        <img src="" alt="" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
