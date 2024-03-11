f4385e56065abedca3b335b71d8267209f34b829
AnthonyLaw <anthonylaw910@gmail.com>
2024-03-11 11:06:28 -0600

    cards

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
   return (
     <div className="page">
       <div className="page__content">
         <Header />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
 function Header() {
   return (
     <header className="header">
-      <img src="" alt="" className="header__logo" />
+      <img src={logo} alt="" className="header__logo" />
       <p className="header__date-and-location">DATE, LOCATION</p>
+
       <button className="header__add-clothes-btn">+ Add clothes</button>
       <div className="header__user-container">
         <p className="header__username">NAME</p>
-        <img src="" alt="" className="header__avatar" />
+        <img src={avatar} alt="Terrance" className="header__avatar" />
       </div>
     </header>
   );
