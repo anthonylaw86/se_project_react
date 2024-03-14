b1c4f73458467d8f20b9dd7b517d2d0037724442
AnthonyLaw <anthonylaw910@gmail.com>
2024-03-14 12:33:15 -0600

    API requests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -36,3 +36,10 @@ export const defaultClothingItems = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 ];
+
+export const coordinates = {
+  latitude: 40.760781,
+  longitude: -111.891045,
+};
+
+export const APIkey = "6f1c858811040adc049c9e0e4faa4d9d";
