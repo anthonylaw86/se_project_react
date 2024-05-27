b72436f3d9614b65372c528c350183573f52e62b
AnthonyLaw <anthonylaw910@gmail.com>
2024-05-27 15:25:38 -0600

    project 14

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -106,7 +106,7 @@ function App() {
           .addCardLike(id, token)
           .then((updatedCard) => {
             setClothingItems((cards) =>
-              cards.map((item) => (item._id === id ? updatedCard : item))
+              cards.map((item) => (item._id === id ? updatedCard.data : item))
             );
           })
           .catch((err) => console.log(err))
@@ -114,7 +114,7 @@ function App() {
           .removeLike(id, token)
           .then((updatedCard) => {
             setClothingItems((cards) =>
-              cards.map((item) => (item._id === id ? updatedCard : item))
+              cards.map((item) => (item._id === id ? updatedCard.data : item))
             );
           })
           .catch((err) => console.log(err));
